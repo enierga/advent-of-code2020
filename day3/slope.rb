98f14c59ad83ab3674059291085f0d3c3a5c03c8
enierga <enierga.vincentkyle033@gmail.com>
2020-12-04 23:56:32 -0500

    completed day 3: Ruby

--- day3/slope.rb
+++ day3/slope.rb
@@ -1,31 +1,45 @@
 class TreeCounter 
-  # use command line to pass in file as argument
   FILENAME = ARGV[0]
-  @trees = 0
-  @open = 0
-  @rowIndex = 2
-  @currentRow = 0
+  
+  def bruh (x, y)
+    @trees = 0
+    @rowIndex = 0
+    @currentRow = 0
 
-  File.readlines(FILENAME).each do |line|
-    @row = line.split('')
-    if @currentRow != 0
-      if @rowIndex > 30
-        @rowIndex = @rowIndex % 30
+    File.readlines(FILENAME).each do |line|
+      @row = line.split('')
+      
+      # check here for doing correct number of steps down
+      if @currentRow % y == 0
+        if @rowIndex > 30
+          # subtract 1 for 0 indexing
+          @rowIndex = @rowIndex % 30 - 1
+        end
+        if @row[@rowIndex] == '#'
+          @row[@rowIndex] = 'X'
+          @trees += 1
+        else
+          @row[@rowIndex] = 'O'
+        end
+        @rowIndex += x
       end
-
-      if @row[@rowIndex] == '#'
-        @row[@rowIndex] = 'X'
-        @trees += 1
-      else
-        @row[@rowIndex] = 'O'
-        @open += 1
-      end
-      @rowIndex += 3
+      @currentRow += 1
     end
-    @currentRow += 1
-
-    puts "#{@row}"
-    # puts "Row:#{@currentRow} Column:#{@rowIndex} Spot:#{@row[@rowIndex]}"
+    @trees
   end
-  puts "Number of trees:#{@trees} Number of open spaces:#{@open}"
+
 end
+
+# separated here for debugging
+tc = TreeCounter.new
+r1d1 = tc.bruh(1,1)
+r3d1 = tc.bruh(3,1)
+r5d1 = tc.bruh(5,1)
+r7d1 = tc.bruh(7,1)
+r1d2 = tc.bruh(1, 2)
+puts "r1d1. Number of trees: #{r1d1}"
+puts "r3d1. Number of trees: #{r3d1}"
+puts "r5d1. Number of trees: #{r5d1}"
+puts "r7d1. Number of trees: #{r7d1}"
+puts "r1d2. Number of trees: #{r1d2}"
+puts "Product = #{r1d1*r3d1*r5d1*r7d1*r1d2}"
